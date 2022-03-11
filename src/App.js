21dccca16f4dfbdbbaacb9041064d6b77713d2aa
harvinder52 <85876365+harvinder52@users.noreply.github.com>
2022-03-11 15:38:27 +0530

    some background changes, updated screesnhot, cleaned code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [inputPegIndex, setInputPegIndex] = useState([9, 9, 9, 9]);
-  const [hints, setHints] = useState(['-','-','-','-'])
+  const [hints, setHints] = useState(["-", "-", "-", "-"]);
   const baseURL = "http://localhost:8080/submit";
   const postURL = "http://localhost:8080/verifyGuess";
   const tableBody = useRef();
@@ -18,14 +18,14 @@ function App() {
 
     axios.post(postURL, inputPegIndex).then((response) => {
       console.log(response.data);
-      setHints(response.data)
+      setHints(response.data);
+     
     });
-    setInputPegIndex([9,9,9,9])
-    setRowIndex(rowIndex + 1);
-    console.log(document.querySelector('.hintsPegs1').children)
-
+     setInputPegIndex([9, 9, 9, 9]);
+      setRowIndex(rowIndex + 1);
+      console.log(document.querySelector(".hintsPegs1").children);
   };
-  
+
   const [rowIndex, setRowIndex] = useState(0);
   const [colorIndex, setColorIndex] = useState(0);
   let arrayColor = ["Red", "Yellow", "Green", "Blue", "Magenta", "Black"];
@@ -37,19 +37,21 @@ function App() {
         "rowIndex altered",
         tableBody.current.children[rowIndex].children
       );
-      for(let i = 0; i<=9; i++){
-         for (const peg of tableBody.current.children[rowIndex].children) {
-        peg.style['pointer-events'] = 'auto';
+      for (let i = 0; i <= 9; i++) {
+        for (const peg of tableBody.current.children[rowIndex].children) {
+          peg.style["pointer-events"] = "auto";
         }
 
-        if(i!==rowIndex){
+        if (i !== rowIndex) {
           for (const peg of tableBody.current.children[i].children) {
-        peg.style['pointer-events'] = 'none';
+            peg.style["pointer-events"] = "none";
+          }
         }
-        
       }
-      }
-      
+    }else{
+       window.location.reload();
+
+
     }
   }, [rowIndex]);
 
@@ -63,8 +65,6 @@ function App() {
   };
 
   function handleColorPeg(event) {
-    document.querySelector("#row1").style.backgroundColor =
-      event.target.getAttribute("value");
     console.log("handleColorPeg ran", event.target.getAttribute("value"));
   }
   function handleInputPeg(event) {
@@ -81,12 +81,16 @@ function App() {
   }
   useEffect(() => {
     console.log(hints, "hints recieved");
-    if(rowIndex-1>=0){
-    hints.forEach((hint, index)=>{
-      document.querySelector(`.hintsPegs${rowIndex-1}`).children[index].classList.add(hint==='-'?"hintPegRed":"hintPegBlack")
-         
-      //.classList.add(".hintPegRed")
-    })
+    if (rowIndex - 1 >= 0) {
+      hints.forEach((hint, index) => {
+        document
+          .querySelector(`.hintsPegs${rowIndex - 1}`)
+          .children[index].classList.add(
+            hint === "-" ? "hintPegRed" : "hintPegBlack"
+          );
+
+        //.classList.add(".hintPegRed")
+      });
     }
   }, [hints]);
   useEffect(() => {
@@ -101,9 +105,35 @@ function App() {
   }, [colorIndex]);
   return (
     <div className="App">
+      <div className="wrapper">
+        <div className="modal">
+          
+          welcome
+          <button
+            onClick={() => {
+              document.querySelector(".wrapper").style.display = "none";
+                           }}
+          >
+            PLAY NOW!
+          </button>
+        </div>
+      </div>
+      <div className="area">
+        <ul className="circles">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </ul>
+      </div>
       <div className="header">MASTERMIND</div>
       <div className="boardContainer">
-        {("GUESS:", rowIndex)}
         <table>
           <tbody ref={tableBody}>
             {[...Array(10)].map((_, i) => (
@@ -130,7 +160,10 @@ function App() {
                     className="colorPeg"
                   ></td>
                   <td>
-                    <div className={`hintsPegs${i}`} style={{ float: "left", width: "50%", height: "50%" }}>
+                    <div
+                      className={`hintsPegs${i}`}
+                      style={{ float: "left", width: "50%", height: "50%" }}
+                    >
                       <span className="hintPeg "></span>
                       <span className="hintPeg "></span>
                       <span className="hintPeg "></span>
@@ -157,7 +190,11 @@ function App() {
               </li>
             ))}
             <li>
-              <button className="checkButton" onClick={onButtonCheckGuess} disabled={inputPegIndex.includes(9)?true:false}>
+              <button
+                className="checkButton"
+                onClick={onButtonCheckGuess}
+                disabled={inputPegIndex.includes(9) ? true : false}
+              >
                 Check
               </button>
             </li>
